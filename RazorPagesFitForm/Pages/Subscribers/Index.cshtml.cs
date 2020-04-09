db4ef1c2dfce117b5f6c57f0fca445a3fd33acde
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-09 00:58:23 -0400

    committing changes, added search function for name

--- RazorPagesFitForm/Pages/Subscribers/Index.cshtml.cs
+++ RazorPagesFitForm/Pages/Subscribers/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesFitForm.Data;
 using RazorPagesFitForm.Models;
@@ -19,11 +20,24 @@ namespace RazorPagesFitForm.Pages.Subscribers
             _context = context;
         }
 
-        public IList<Models.Subscribers> Subscribers { get;set; }
-
         public async Task OnGetAsync()
         {
-            Subscribers = await _context.Subscribers.ToListAsync();
+            var subscribers = from s in _context.Subscribers
+                select s;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                subscribers = subscribers.Where(s => s.Name.Contains(SearchString));
+            }
+
+            Subscribers = await subscribers.ToListAsync();
         }
+
+        public IList<Models.Subscribers> Subscribers { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Names { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
     }
 }
