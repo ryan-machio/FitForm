e308c4d8db0527b8ac473781c5fe05c3f0739c31
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-08 23:13:10 -0400

    initial commit

---
+++ RazorPagesFitForm/Models/Subscribers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace RazorPagesFitForm.Models
+{
+    public class Subscribers
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime SubDate{ get; set; }
+        
+    }
+}
