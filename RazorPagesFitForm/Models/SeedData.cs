ab4e97866f54ed751fb9c506f5ac7c36e23aa8b5
Ryan Manchanthasouk <ryan.manch@outlook.com>
2020-04-08 23:54:13 -0400

    seeded database

---
+++ RazorPagesFitForm/Models/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesFitForm.Data;
+using System;
+using System.Linq;
+using RazorPagesFitForm.Models;
+
+namespace RazorPagesFitForm
+{ 
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesFitFormContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesFitFormContext>>()))
+            {
+                if (context.Subscribers.Any())
+                {
+                    return; //DB has been seeded
+                }
+
+                context.Subscribers.AddRange(
+                    new Subscribers()
+                    {
+                        Name = "Luke",
+                        Email = "lukethao@outlook.com",
+                        SubDate = DateTime.Parse("2020-01-01")
+                    });
+            }
+            
+        }
+    }
+}
